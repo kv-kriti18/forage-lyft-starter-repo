a8e5a2992407f19fb503f98194f7b5460c6e3b8d
kv-kriti18 <114588308+kv-kriti18@users.noreply.github.com>
2023-07-10 10:48:20 +0530

    added battery classes

---
+++ battery/spindler.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from car import Car
+
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def battery_should_be_serviced(self):
+        return self.current_date - self.last_service_date >= 2*365
