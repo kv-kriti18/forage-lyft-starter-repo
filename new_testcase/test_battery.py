85c63f255e867d86950b1bd2868b04df96c05a18
kv-kriti18 <114588308+kv-kriti18@users.noreply.github.com>
2023-07-11 11:43:56 +0530

    new test cases for battery and engine classes

---
+++ new_testcase/test_battery.py
@@ -0,0 +1,40 @@
+import unittest
+from datetime import datetime
+
+from battery.nubbin import NubbinBattery
+from battery.spindler import SpindlerBattery
+
+class TestSpindler(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        car = SpindlerBattery(today,last_service_date)
+        self.assertTrue(car.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+
+        car = SpindlerBattery(today,last_service_date)
+        self.assertFalse(car.needs_service())
+
+class TestNubin(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+
+        car = NubbinBattery(today,last_service_date)
+        self.assertTrue(car.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        car = NubbinBattery(today,last_service_date)
+        self.assertFalse(car.needs_service())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
