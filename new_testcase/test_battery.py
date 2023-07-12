33b161a1fc61fc973ca60fee26986ab7350e4295
kv-kriti18 <114588308+kv-kriti18@users.noreply.github.com>
2023-07-12 14:18:23 +0530

    spindler battery service time increased to 3 years

--- new_testcase/test_battery.py
+++ new_testcase/test_battery.py
@@ -7,14 +7,14 @@ from battery.spindler import SpindlerBattery
 class TestSpindler(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
 
         car = SpindlerBattery(today,last_service_date)
         self.assertTrue(car.needs_service())
 
     def test_battery_should_not_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 1)
+        last_service_date = today.replace(year=today.year - 2)
 
         car = SpindlerBattery(today,last_service_date)
         self.assertFalse(car.needs_service())
