6525a10e78649c911fc642bf6ba872e734f9ae99
kv-kriti18 <114588308+kv-kriti18@users.noreply.github.com>
2023-07-12 14:46:37 +0530

    tire test

---
+++ new_testcase/test_tire.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import datetime
+
+from engine.model.calliope import Calliope
+from engine.model.glissade import Glissade
+from engine.model.palindrome import Palindrome
+from engine.model.rorschach import Rorschach
+from engine.model.thovex import Thovex
+
+class TestCalliope(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        current_mileage = 0
+        last_service_mileage = 0
+
+        car = Calliope(last_service_date, current_mileage, last_service_mileage)
+        self.assertTrue(car.needs_service())
