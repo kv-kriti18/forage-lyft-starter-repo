d7ddc577877ecf025bbf5af7a89db58a83e08748
kv-kriti18 <114588308+kv-kriti18@users.noreply.github.com>
2023-07-12 14:40:43 +0530

    tire functionality

---
+++ service/tire.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+from car import Car
+
+import numpy as np
+
+class Carrigan(Car, ABC):
+    
+ x1 = np.arange(1)  
+
+ return x1
+
+ class Octoprime(Car, ABC):
+    
+ x2 = np.arange(1)  
+
+ return x2
